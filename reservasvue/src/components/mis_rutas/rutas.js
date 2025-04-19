fa30b5f7470487fa1a5c891668ed0fcd0067c187
nicokrafterra <gamerjuan442@gmail.com>
2025-04-19 17:53:58 -0500

    adsadasd

--- reservasvue/src/components/mis_rutas/rutas.js
+++ reservasvue/src/components/mis_rutas/rutas.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Perfil from "../Perfil.vue";
+
 import Reserva from "../Reserva.vue";
 import Platos from "../Platos.vue";
 import Pricipal from "../Pricipal.vue";
@@ -36,11 +36,6 @@ const routes = [
         path: "/index",
         name: "index",
         component: Pricipal
-    },
-    {
-        /*path: "/Perfil",
-        name: "Perfil",
-        component: Perfil*/
     },
     {
         path: "/Reservas",
@@ -180,7 +175,7 @@ const routes = [
     {
         path: "/NequiP",
         name: "nequiP",
-        component: NeqP
+        component: NequiP
     }
     
 ];
